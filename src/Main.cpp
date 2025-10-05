26b26bfab509854f39df34b0382351a92952bb5c
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-10-05 19:12:19 +0200

    readded debug output

--- src/Main.cpp
+++ src/Main.cpp
@@ -68,7 +68,7 @@ Main::Main(unsigned int _width, unsigned int _height){
     Console::PrintLine("Renderer", glGetString(GL_RENDERER));
     Console::PrintLine("Version", glGetString(GL_VERSION));
 
-    /*int flags;
+    int flags;
     glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
     if(flags & GL_CONTEXT_FLAG_DEBUG_BIT){
         Console::PrintLine("initialise debug output...");
@@ -76,7 +76,7 @@ Main::Main(unsigned int _width, unsigned int _height){
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
         glDebugMessageCallback(glDebugOutput, nullptr);
         glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
-    }*/
+    }
 
     //vsync
     SDL_GL_SetSwapInterval(int(vsync_on));
