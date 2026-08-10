bd33e511b60af2e4e40a8e3ebfba3c963c77de2a
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2026-08-10 06:07:11 +0200

    removed some debug print

--- src/collision_grid.cpp
+++ src/collision_grid.cpp
@@ -60,9 +60,9 @@ void CollisionGrid::OnSpawn(){
 
     for(auto&& actor : actors){
         std::vector<int> divisions_for_actor = GetDivisionsForRectangle(actor->GetRectangle());
-        Console::PrintLine("Actor rect",actor->editor_name,actor->GetRectangle().position,actor->GetRectangle().size);
+        //Console::PrintLine("Actor rect",actor->editor_name,actor->GetRectangle().position,actor->GetRectangle().size);
         for(const int index : divisions_for_actor){
-            Console::PrintLine("Actor added to grid",actor->editor_name);
+            //Console::PrintLine("Actor added to grid",actor->editor_name);
             divisions[index].push_back(actor.get());
         }
     }
@@ -80,7 +80,7 @@ void CollisionGrid::OnUpdate([[maybe_unused]] float _delta_time){
         //if(former_division == Errors::OUT_OF_BOUNDS) continue;
 
         if(new_divisions_for_actor != former_divisions_for_actor || u_actor->is_dead){
-            Console::PrintLine(u_actor->GetRectangle().position, u_actor->former_rectangle.position);
+            //Console::PrintLine(u_actor->GetRectangle().position, u_actor->former_rectangle.position);
 
             //Unnecessary if the new division is going to be reordered anyway. This could be solved
             // partly by keeping track of whether things are removed from the division or not.
@@ -101,7 +101,7 @@ void CollisionGrid::OnUpdate([[maybe_unused]] float _delta_time){
         divisions[i].clear();
     }
 
-    if(!actors_to_relocate.empty()) Console::PrintLine(actors_to_relocate.size());
+    //if(!actors_to_relocate.empty()) Console::PrintLine(actors_to_relocate.size());
 
     for(Actor* actor : actors_to_relocate){
         if(!actor->is_dead){
